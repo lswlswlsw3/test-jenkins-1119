894a86308c5071ee8c08a7add7f90088a0bafa41
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-11-19 10:43:37 +0900

    jenkins test를 위함

---
+++ src/main/java/com/woongs/WoongTestJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.woongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoongTestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoongTestJenkinsApplication.class, args);
+	}
+}
